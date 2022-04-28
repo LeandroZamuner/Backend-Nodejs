ad4182ca3b32a6dab6004ff8c5bb39491558c7d5
LeandroZamuner <leandrozamuner96@gmail.com>
2022-04-28 09:10:12 -0300

    removed a router constant that was not used

--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -1,4 +1,3 @@
-const { Router } = require('express');
 const express = require('express');
 const settingsRouter = require('../services/settingsService');
 const validatorHandler = require('../middlewares/validatorHandler');
